5582b4c8b6aed2c3f4abfd74009bf4a454c38667
jxp603 <janicepeng091@gmail.com>
2017-05-19 11:55:49 -0700

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -13,6 +13,7 @@ with(sub,{
 	plot(sub$Global_active_power,type="l",ylab="Global Active Power(kilowatts)")
 	plot(sub$Voltage,typle="l",xlab="datetime",ylab="Voltage")
 	matplot(sub[7:9],type="l",ylab="Energy sub Metering")
+	legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 	plot(sub$Global_reactive_power,typle="l",xlab="datetime",ylab="Global_reactive_power")
 })
 png(filename='plot4.png', width=480, height=480, units='px')
