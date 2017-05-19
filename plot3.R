e1ddca65091c8c9b5ae300f736123200dafed4fb
jxp603 <janicepeng091@gmail.com>
2017-05-19 11:56:02 -0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -9,4 +9,5 @@ sub<-rbind(sub1,sub2)
 
 #plot3
 matplot(sub[7:9],type="l")
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 png(filename='plot3.png', width=480, height=480, units='px')
